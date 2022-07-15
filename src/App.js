27c3d8cf4894190ab586bd150b6fddfc85baef13
horpemee <arifoopeyemi@gmail.com>
2022-07-16 00:29:36 +0100

    mdified app.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-const Content = () => {
+import React, { useState } from "react";
+
+const btnClick = document.querySelector(".submit");
+
+const App = () => {
   const mcqs = [
     {
       id: 1,
@@ -33,38 +37,43 @@ const Content = () => {
       option4: "none of the above",
     },
   ];
-};
 
-const App = ({ mcq }) => {
-  const { question, option1, option2, option3, option4 } = mcq;
+  const [submit, setSubmit] = useState("");
+  function generateMcq() {
+    // btnClick.addEventListener("click", () => {
+    let newMcq = Math.floor(Math.random() * mcqs.length + 0);
+    // });
 
+    setSubmit(newMcq);
+  }
+  // declaring a variable index
   return (
     <div className="wrapper">
-      {mcqs.map((mcq) => (
-        <div className="quiz-container">
-          <h1 className="question1">{question}</h1>
-          <input type="radio" className="java" value="java" name="lang" />
-          <label for="java">Java{option1}</label>
-          <br />
-          <input type="radio" className="c" value="c" name="lang" />
-          <label for="c">{option2}</label>
-          <br />
-          <input type="radio" className="python" value="python" name="lang" />
-          <label for="python">{option3}</label>
-          <br />
-          <input
-            type="radio"
-            className="javascript"
-            value="javascript"
-            name="lang"
-          />
-          <label for="javascript">{option4}</label>
-          <br />
-        </div>
-      ))}
+      <div className="quiz-container">
+        <h1 className="question1">{mcqs[0].question}</h1>
+        <input type="radio" className="java" value="java" name="lang" />
+        <label for="java">{mcqs[0].option1}</label>
+        <br />
+        <input type="radio" className="c" value="c" name="lang" />
+        <label for="c">{mcqs[0].option2}</label>
+        <br />
+        <input type="radio" className="python" value="python" name="lang" />
+        <label for="python">{mcqs[0].option3}</label>
+        <br />
+        <input
+          type="radio"
+          className="javascript"
+          value="javascript"
+          name="lang"
+        />
+        <label for="javascript">{mcqs[0].option4}</label>
+        <br />
+        <button className="submit" onClick={generateMcq}>
+          Submit
+        </button>
+      </div>
       ;
     </div>
   );
 };
-
 export default App;
